9f74bc48a634ec50c6ede690e400ad29f4caafad
lucas br <rodri@PRODUCAO6.musical.net>
2023-12-05 17:34:10 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -33,6 +33,24 @@ app.post('/criar' , (requisicoes, resposta) => {
 })
 
 app.get('/', (requisicao, resposta) => {
+    const sql = 'SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) => {
+       if (erro) {
+        return console.log(erro)
+       } 
+
+       const tarefas = dados.map((dado) => {
+        return {
+            id: dado.id,
+            descricao: dado.descricao,
+            completa: dado.completa === 0 ? false: 0
+        }
+       })
+
+       
+    })
+
     resposta.render('home')
 })
 
