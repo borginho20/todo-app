bed0900724d3321f6699d3f1ded1ef1ff91f75a5
lucas br <rodri@PRODUCAO6.musical.net>
2023-12-05 00:55:33 -0300

    primeiro execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisiçao, resposta) => {
+    resposta.send("Olá, mundo!")
+})
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
