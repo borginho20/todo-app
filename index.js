4434731c39387a1e596e0bdff87ee696dadec734
lucas br <rodri@PRODUCAO6.musical.net>
2023-12-05 01:46:47 -0300

    criação primeira view do projeto

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const exphbs = require('express-handlebars')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engune())
+app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisiçao, resposta) => {
-    resposta.send("Olá, mundo!")
+    resposta.render('home')
 })
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
